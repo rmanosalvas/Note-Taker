af72cab4326442ce780bdec82bdb46a59e0ccce9
Ricardo <manosalvas2013@gmail.com>
2020-10-12 19:27:13 -0400

    final

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,7 +8,7 @@ const db = require('../db/db.json');
 module.exports = function(app) {
     app.get("/api/notes", (req, res) => {
         console.log('***************');
-        res.sendFile(path.join(path.join(__dirname, "../notes"), "./db/db.json"));
+        res.sendFile(path.join(path.join(__dirname, "../notes"), "../db/db.json"));
         console.log(db)
         console.log('***************');
     });
@@ -17,7 +17,7 @@ module.exports = function(app) {
         // New note
         var newNote = req.body;
         // Load existing DB
-        var existingNotes = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+        var existingNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
         // Add ID to note
         var noteID = (existingNotes.length).toString();
         // Add ID to object
@@ -25,7 +25,7 @@ module.exports = function(app) {
         // Add note to existing
         existingNotes.push(newNote);
         // Overwrite DB with the new file
-        fs.writeFileSync("../db/db.json", JSON.stringify(existingNotes));
+        fs.writeFileSync("./db/db.json", JSON.stringify(existingNotes));
         // return the new note in the res
         res.json(newNote);
         console.log("New note " + newNote.title + " added to notes DB")
@@ -33,7 +33,7 @@ module.exports = function(app) {
 
     app.delete("/api/notes/:id", (req, res) => {
         // load existing DB
-        var existingNotes = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+        var existingNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
         console.log(existingNotes);
         // get the ID
         var noteID = req.params.id;
